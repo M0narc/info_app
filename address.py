fd81502931df0418fdb30bf0c121ecf903591337
M0narc <francomatiaslujan23@gmail.com>
2024-08-15 13:47:55 -0300

    estructura basica para la agenda

---
+++ address.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+
+
+
+# Función para mostrar la hora actual
+
+
+# Función para agregar un contacto
+
+
+# Función para eliminar un contacto
+
+
+# Función para mostrar la ventana modal con la información del contacto
+
+
+
+# Función para salir de la aplicación
+def salir():
+    ventana.destroy()
+
+# Función para limpiar la lista de contactos
+
+
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title('Agenda de Contactos')
+ventana.geometry('500x500')
+
+# Crear el menú desplegable
+
+
+# Crear el reloj en la parte superior derecha
+
+
+# Marco para el boton de nombre
+
+
+
+# Crear las entradas para los detalles del contacto
+
+
+# Marco para los botones de telefono y direccion
+
+
+# Botón para agregar un contacto
+
+
+# Crear el marco para la lista de contactos con scrollbar
+
+
+# Crear un marco para contener los botones de abajo
+
+
+# Botón para eliminar un contacto
+
+
+# Botón para mostrar la información detallada del contacto
+
+
+# Iniciar el bucle de la ventana
+ventana.mainloop()
